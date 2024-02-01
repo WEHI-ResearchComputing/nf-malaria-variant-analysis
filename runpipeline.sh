e638924aedb9f4eae72a40c29c9944164d7f6c20
Julie Iskander <iskander.j@wehi.edu.au>
2024-02-01 11:14:50 +1100

    remove clean up cmd

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 module purge
 module load miniconda3/latest nextflow/23.10.0  apptainer/1.0.0 
-rm -rf /vast/scratch/users/$USER/results_sh_400000
 nextflow run main.nf -profile milton  -config config/nextflow_sh.config
