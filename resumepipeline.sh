135e92d894c2727b078a48f3a85935367c3b5756
Jocelyn Penington <penington.j@wehi.edu.au>
2025-01-16 11:00:42 +1100

    run and resume scripts: add input variable

--- resumepipeline.sh
+++ resumepipeline.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo Additional options can be added to nextflow by enclosing in quotes.
+echo E.g. ./resumepipeline \'sad_nobel -c path-to-task-config\'
+
 module purge
 module load nextflow/23.10.0
-nextflow run main.nf -profile apptainer -resume
+nextflow run main.nf -profile apptainer -resume $1
--- runpipeline.sh
+++ runpipeline.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 module purge
+echo Additional options can be added to nextflow by enclosing in quotes.
+echo E.g. ./runpipeline \'-c path-to-task-config\'
 module load nextflow/23.10.0
-nextflow run main.nf -profile apptainer
+nextflow run main.nf -profile apptainer $1
