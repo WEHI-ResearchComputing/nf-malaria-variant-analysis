e86f89360df639592720aece42df53dc19238c6e
Julie Iskander <iskander.j@wehi.edu.au>
2025-07-09 21:49:40 +1000

    update nextflow version

--- runpipeline.sh
+++ runpipeline.sh
@@ -2,5 +2,5 @@
 module purge
 echo Additional options can be added to nextflow by enclosing in quotes.
 echo E.g. ./runpipeline \'-c path-to-task-config\'
-module load nextflow/24.04.2 
+module load nextflow/25.04.2
 nextflow run main.nf -profile apptainer $1
